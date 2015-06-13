81a95799e117ad951fe99b15253f32dabbd8db91
Kevin Qiu <gangqiu0@gmail.com>
2015-06-13 16:04:47 -0400

    properly sets up form

--- solution_code/scripts/script.js
+++ solution_code/scripts/script.js
@@ -41,8 +41,13 @@
             var numPlayers = event.target.value;
 
             renderFormForPlayers(numPlayers);
+        });
 
-            $('#start-game-btn').removeClass('hidden');
+        $('#player-names').on('keyup', '.username', function () {
+            console.log('adf');
+            if (isPlayersFieldsValid()) {
+                $('#start-game-btn').removeClass('hidden');
+            }
         });
 
         $('#start-game-btn').click(function () {
@@ -67,6 +72,20 @@
             console.log($template);
             $formContainer.append($template);
         }
+
+
+    };
+
+    var isPlayersFieldsValid = function () {
+        var isValid = true;
+
+        $('.username').each(function () {
+            if ($(this).val() === '') {
+                isValid = false
+            };
+        });
+
+        return isValid;
     };
 
 
