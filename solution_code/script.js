336c26cfaea9ce00d41d985e5a068bbe38789305
Kevin Qiu <gangqiu0@gmail.com>
2015-06-13 14:57:00 -0400

    sets up script.js

---
+++ solution_code/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var $ = window.$;
+var map;
+var worldCapitalsUrl = 'http://techslides.com/demos/country-capitals.json';
+
+var createMap = function () {
+    map = L.map('map').setView([51.505, -0.09], 2);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 2,
+        maxZoom: 2
+    }).addTo(map);
+};
+
+var listenToMapClicks = function () {
+
+};
+
+$(function () {
+    createMap();
+    
+    map.on('click', function (event) {
+        var latLng = event.latlng;
+        console.log(latLng);
+    });
+});
