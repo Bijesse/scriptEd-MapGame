c6bdccd6c5a2c50e3b77abf998f2a99841bd9e46
Stevenson Michel <thoven87@gmail.com>
2015-06-13 15:01:44 -0400

    Get all countries and added a function to get a random country capital

--- solution_code/scripts/api.js
+++ solution_code/scripts/api.js
@@ -2,18 +2,28 @@
 
   var countryCapitals = (countryCapitals || []);
 
-
-  $.getJSON(worldCapitalsUrl, function(data) {
+  /**
+   * Assign countryCapitals with the returned data
+   * @param  {Array} data A list of countries
+   */
+  var processData = function processData(data) {
     countryCapitals = data;
-  });
+  };
 
-  var randomCity = function randomCity() {
+  // Call the api to get all counties with their capital
+  $.getJSON('https://restcountries.eu/rest/v1/all', processData);
+
+  /**
+   * Returns a randomCapital
+   * @return {String} a Country capital
+   */
+  var randomCapital = function randomCapital() {
 
     var random = Math.round(Math.random() * countryCapitals.length);
 
-    return countryCapitals[random];
+    return countryCapitals[random].capital;
   };
 
-  window.randomCity = randomCity;
+  window.randomCapital = randomCapital;
 
 }(jQuery));
